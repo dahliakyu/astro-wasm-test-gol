72019d2fc0b8ab132ca3af8cd09ceb8058891962
dahliakyu <lamiyavi@hotmail.com>
2023-03-20 17:40:57 +0100

    add local dependency

---
+++ pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead = 0,
+  Alive = 1,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {string}
+*/
+  render(): string;
+}
